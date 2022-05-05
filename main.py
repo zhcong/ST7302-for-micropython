5606b980b3c7ab04e3fa215e7ce2bd9d41ec55a1
zhcong <zhcong@users.noreply.github.com>
2022-05-05 18:48:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ spi=SPI(id=1, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(10, Pin.OUT), mo
 dc_pin=Pin(0,Pin.OUT)
 rest_pin=Pin(1,Pin.OUT,Pin.PULL_UP)
 
-screen = Screen.ST7032(spi, dc_pin, rest_pin, 250, 122)
+screen = Screen.ST7302(spi, dc_pin, rest_pin, 250, 122)
 screen.text('hello world', 5, 5)
 screen.fill_rect(5, 20, 10, 20, 1)
 screen.text('hello2 world2', 150, 50)
